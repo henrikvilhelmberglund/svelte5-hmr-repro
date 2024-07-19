462ba5824c6e5ce60eac4d93fffc36a52a4521f9
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-07-19 15:55:13 +0200

    feat: first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>
+  line 1
+</p>
+
+<p>
+  line 2
+</p>
+
+<p>
+  line 3
+</p>
